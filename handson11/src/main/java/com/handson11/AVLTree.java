efc5af00f5c93e1e3055b244ed12ae793d3eabad
Nikethan Reddy Ganampet <nikethanreddyganampet@gmail.com>
2024-04-02 19:10:25 -0500

    handson11 -AVLTree

---
+++ handson11/src/main/java/com/handson11/AVLTree.java
@@ -0,0 +1,197 @@
+package com.handson11;
+
+class AVLNode {
+    int data, height;
+    AVLNode left, right;
+
+    public AVLNode(int data) {
+        this.data = data;
+        this.height = 1;
+        this.left = null;
+        this.right = null;
+    }
+}
+
+class AVLTree {
+    AVLNode root;
+
+    int height(AVLNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return node.height;
+    }
+
+    int max(int a, int b) {
+        return (a > b) ? a : b;
+    }
+
+    AVLNode rightRotate(AVLNode y) {
+        AVLNode x = y.left;
+        AVLNode T2 = x.right;
+
+        x.right = y;
+        y.left = T2;
+
+        y.height = max(height(y.left), height(y.right)) + 1;
+        x.height = max(height(x.left), height(x.right)) + 1;
+
+        return x;
+    }
+
+    AVLNode leftRotate(AVLNode x) {
+        AVLNode y = x.right;
+        AVLNode T2 = y.left;
+
+        y.left = x;
+        x.right = T2;
+
+        x.height = max(height(x.left), height(x.right)) + 1;
+        y.height = max(height(y.left), height(y.right)) + 1;
+
+        return y;
+    }
+
+    int getBalance(AVLNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return height(node.left) - height(node.right);
+    }
+
+    AVLNode insert(AVLNode node, int data) {
+        if (node == null) {
+            return (new AVLNode(data));
+        }
+
+        if (data < node.data) {
+            node.left = insert(node.left, data);
+        } else if (data > node.data) {
+            node.right = insert(node.right, data);
+        } else {
+            return node;
+        }
+
+        node.height = 1 + max(height(node.left), height(node.right));
+
+        int balance = getBalance(node);
+
+        if (balance > 1 && data < node.left.data) {
+            return rightRotate(node);
+        }
+
+        if (balance < -1 && data > node.right.data) {
+            return leftRotate(node);
+        }
+
+        if (balance > 1 && data > node.left.data) {
+            node.left = leftRotate(node.left);
+            return rightRotate(node);
+        }
+
+        if (balance < -1 && data < node.right.data) {
+            node.right = rightRotate(node.right);
+            return leftRotate(node);
+        }
+
+        return node;
+    }
+
+    AVLNode minValueNode(AVLNode node) {
+        AVLNode current = node;
+
+        while (current.left != null) {
+            current = current.left;
+        }
+
+        return current;
+    }
+
+    AVLNode deleteNode(AVLNode root, int data) {
+        if (root == null) {
+            return root;
+        }
+
+        if (data < root.data) {
+            root.left = deleteNode(root.left, data);
+        } else if (data > root.data) {
+            root.right = deleteNode(root.right, data);
+        } else {
+            if ((root.left == null) || (root.right == null)) {
+                AVLNode temp = null;
+                if (temp == root.left) {
+                    temp = root.right;
+                } else {
+                    temp = root.left;
+                }
+
+                if (temp == null) {
+                    temp = root;
+                    root = null;
+                } else {
+                    root = temp;
+                }
+            } else {
+                AVLNode temp = minValueNode(root.right);
+                root.data = temp.data;
+                root.right = deleteNode(root.right, temp.data);
+            }
+        }
+
+        if (root == null) {
+            return root;
+        }
+
+        root.height = max(height(root.left), height(root.right)) + 1;
+
+        int balance = getBalance(root);
+
+        if (balance > 1 && getBalance(root.left) >= 0) {
+            return rightRotate(root);
+        }
+
+        if (balance > 1 && getBalance(root.left) < 0) {
+            root.left = leftRotate(root.left);
+            return rightRotate(root);
+        }
+
+        if (balance < -1 && getBalance(root.right) <= 0) {
+            return leftRotate(root);
+        }
+
+        if (balance < -1 && getBalance(root.right) > 0) {
+            root.right = rightRotate(root.right);
+            return leftRotate(root);
+        }
+
+        return root;
+    }
+
+    void inorder(AVLNode node) {
+        if (node != null) {
+            inorder(node.left);
+            System.out.print(node.data + " ");
+            inorder(node.right);
+        }
+    }
+
+    public static void main(String[] args) {
+        AVLTree tree = new AVLTree();
+
+        tree.root = tree.insert(tree.root, 10);
+        tree.root = tree.insert(tree.root, 90);
+        tree.root = tree.insert(tree.root, 20);
+        tree.root = tree.insert(tree.root, 80);
+        tree.root = tree.insert(tree.root, 30);
+        tree.root = tree.insert(tree.root, 70);
+        tree.root = tree.insert(tree.root, 40);
+
+        System.out.println("Inorder traversal of the constructed AVL tree is:");
+        tree.inorder(tree.root);
+
+        System.out.println("\n\nDelete 10");
+        tree.root = tree.deleteNode(tree.root, 10);
+        System.out.println("Inorder traversal after deletion:");
+        tree.inorder(tree.root);
+    }
+}
